a9a13a3d86a1694c435da7d44bc10019ef4e1c18
dikamjit-borah <dikamjitborah.devacc01@gmail.com>
2024-04-05 22:53:03 +0530

    add util files

---
+++ app/src/main/java/com/hobarb/molio/utils/Constants.kt
@@ -0,0 +1,19 @@
+package com.hobarb.molio.utils
+
+object Constants {
+    object Collection {
+        const val WATCHED = "watched"
+        const val WATCHING = "watching"
+        const val FAVORITES = "favorites"
+        const val WISHLIST = "wishlist"
+        const val CUSTOM = "custom"
+    }
+
+    object Messages {
+        const val NETWORK_ERROR = "Some network error occurred while fetching data"
+        const val SOMETHING_WRONG = "Something went wrong. Please try after some time"
+        const val NO_RESPONSE = "Response does not contain data"
+    }
+
+    const val USER_ID = "USER_ID"
+}
---
+++ app/src/main/java/com/hobarb/molio/utils/Helpers.kt
@@ -0,0 +1,11 @@
+package com.hobarb.molio.utils
+
+class Helpers {
+    fun showLoader() {
+
+    }
+
+    fun hideLoader() {
+
+    }
+}

5da877606ea84c360c215ee05260526499ad646e
dikamjit-borah <dikamjitborah.devacc01@gmail.com>
2024-04-07 21:45:22 +0530

    add searchview, implement call to omdb search api

--- app/src/main/java/com/hobarb/molio/ui/SaveActivity.kt
+++ app/src/main/java/com/hobarb/molio/ui/SaveActivity.kt
@@ -1,24 +1,22 @@
 package com.hobarb.molio.ui
 
+import TitleDetails
 import android.os.Bundle
+import android.util.Log
+import android.widget.SearchView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.gson.JsonElement
 import com.hobarb.molio.R
-import com.hobarb.molio.adapters.TitlesAdapter
 import com.hobarb.molio.databinding.ActivitySaveBinding
-import com.hobarb.molio.models.SaveTitleModel
-import com.hobarb.molio.models.TitleModel
-import com.hobarb.molio.network.RetrofitClient
+import com.hobarb.molio.models.dtos.SaveTitleModel
+import com.hobarb.molio.models.schemas.SearchTitle
 import com.hobarb.molio.services.ApiHandler
 import com.hobarb.molio.utils.Constants
-import com.hobarb.molio.utils.Helpers
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import kotlinx.coroutines.runBlocking
 
 class SaveActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySaveBinding
@@ -33,13 +31,17 @@ class SaveActivity : AppCompatActivity() {
         }
         binding = ActivitySaveBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.submitBtn.setOnClickListener {
+        svTitlesSetup()
+
+        binding.btnSubmit.setOnClickListener {
             try {
                 //Helpers().showLoader()
-                handleSubmitBtn()
+                //handleSubmitBtn()
+                callSearchApi("something")
 
             } catch (ex: Exception) {
                 //Helpers().hideLoader()
+                print("ssmsm")
             }
         }
     }
@@ -47,10 +49,9 @@ class SaveActivity : AppCompatActivity() {
     private fun handleSubmitBtn() {
         ApiHandler().saveTitle(
             body = SaveTitleModel(
-                "some_user",
-                binding.collectionEt.text.toString(),
-                null,
-                TitleModel(binding.titleEt.text.toString())
+                user_id = Constants.USER_ID,
+                collection = binding.etCollection.text.toString(),
+                details = TitleDetails(Title = binding.etTitle.text.toString())
             )
         ) { success, message, data ->
             //hideLoader() // Hide loader after API call is completed
@@ -62,6 +63,49 @@ class SaveActivity : AppCompatActivity() {
                 // Handle failure case
             }
         }
+    }
+
+    private fun svTitlesSetup() {
+        binding.svTitles.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                if (query != null) {
+                    val searchResults =  (query)
+                    if (searchResults != null) {
+                       // setup_rvSearchResults(searchResults)
+                    }
+                }
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                if (newText != null && newText.length > 2) {
+                    val searchResults = callSearchApi(newText)
+                    if (searchResults != null) {
+                        //setup_rvSearchResults(searchResults)
+                    }
+                }
+                return false
+            }
+        })
+    }
+
+    private fun callSearchApi(s: String) {
+            try {
+                ApiHandler().searchTitles(s) { success, message, data ->
+                    //hideLoader() // Hide loader after API call is completed
+                    if (success) {
+                        Toast.makeText(baseContext, message + data, Toast.LENGTH_SHORT).show()
+                        // Proceed to next step
+                    } else {
+                        Toast.makeText(baseContext, message + data, Toast.LENGTH_SHORT).show()
+                        // Handle failure case
+                    }
+                }
+
+            }catch (exception: java.lang.Exception){
+                exception.printStackTrace()
+            }
+
     }
 
 

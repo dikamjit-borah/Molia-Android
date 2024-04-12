98fc38ea9a9f7869c896d6c3a42aa0ef1cf63fc8
dikamjit-borah <dikamjitborah.devacc01@gmail.com>
2024-04-13 03:32:12 +0530

    refactor: SaveActivity ui and code

--- app/src/main/java/com/hobarb/molia/ui/SaveActivity.kt
+++ app/src/main/java/com/hobarb/molia/ui/SaveActivity.kt
@@ -23,7 +23,10 @@ import kotlinx.coroutines.launch
 class SaveActivity : AppCompatActivity(), OnItemClickListener<SearchedTitle> {
     private lateinit var binding: ActivitySaveBinding
     private var searchedTitles: MutableList<SearchedTitle> = mutableListOf()
-    private  lateinit var titleDetails: TitleDetails
+    private var titleDetails: TitleDetails = TitleDetails()
+    private var searchJob: Job? = null
+    private var shouldExecuteQueryTextChange = true
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,27 +37,115 @@ class SaveActivity : AppCompatActivity(), OnItemClickListener<SearchedTitle> {
     }
 
     private fun setupViews() {
+        svTitleSetup()
         btnSubmitSetup()
-        svTitlesSetup()
+    }
+
+    private fun svTitleSetup() {
+        binding.svTitle.setOnQueryTextListener(object : SearchView.OnQueryTextListener,
+            androidx.appcompat.widget.SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                if (shouldExecuteQueryTextChange) {
+                    if (newText?.length!! > 0) {
+                        newText.let { text ->
+                            searchJob?.cancel() // Cancel the previous search job if any
+                            searchJob = CoroutineScope(Dispatchers.Main).launch {
+                                delay(300) // Debounce time in milliseconds
+                                handleSearchEvent(text)
+                            }
+                        }
+
+                    } else {
+                        searchedTitles.removeAll { true }
+                        binding.rvSearchedTitles.adapter?.notifyDataSetChanged()
+                    }
+                }
+                return false
+            }
+        })
+    }
+
+    private fun handleSearchEvent(s: String) {
+        try {
+            ApiHandler().searchTitles(s) { success, message, data ->
+                if (success) {
+                    if (data != null) {
+                        searchedTitles = data as MutableList<SearchedTitle>
+                        rvSearchedTitlesSetup(searchedTitles)
+                    }
+                } else {
+                    Toast.makeText(baseContext, message, Toast.LENGTH_SHORT).show()
+                }
+            }
+        } catch (exception: java.lang.Exception) {
+            Toast.makeText(baseContext, exception.message, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun rvSearchedTitlesSetup(titles: MutableList<SearchedTitle>) {
+        binding.rvSearchedTitles.layoutManager = LinearLayoutManager(this)
+        binding.rvSearchedTitles.adapter = SearchedTitlesAdapter(this, this, titles)
+    }
+
+    override fun onItemClick(item: SearchedTitle) {
+        clearSearchedTitles()
+        fetchTitleDetails(item)
+    }
+
+    private fun clearSearchedTitles() {
+        searchedTitles.clear()
+        binding.rvSearchedTitles.adapter?.notifyDataSetChanged()
+    }
+
+    private fun fetchTitleDetails(title: SearchedTitle) {
+        ApiHandler().fetchTitleDetails(title.imdbID) { success, message, data ->
+            if (success) {
+                if (data != null) {
+                    handleTitleFetchedEvent(data)
+                }
+            } else {
+                Toast.makeText(baseContext, message + data, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    fun handleTitleFetchedEvent(titleDetails: TitleDetails) {
+        this.titleDetails = titleDetails
+        setupEts(titleDetails)
+
+    }
+
+    fun setupEts(title: TitleDetails) {
+        shouldExecuteQueryTextChange = false
+        binding.svTitle.setQuery(title.Title, false)
+        binding.etDirector.setText(title.Director)
+        binding.etActors.setText(title.Actors)
+        binding.etYear.setText(title.Year)
+        binding.etLanguage.setText(title.Language)
+        binding.etImdbRating.setText(title.imdbRating)
+        shouldExecuteQueryTextChange = true
     }
 
     private fun btnSubmitSetup() {
         binding.btnSubmit.setOnClickListener {
             try {
-               handleSubmitBtn()
-
+                val details = getTitleDetails()
+                submitDetails(
+                    details,
+                    binding.etCollection.text.toString(),
+                    binding.etSubCollection.text.toString()
+                )
             } catch (ex: Exception) {
-                Toast.makeText(baseContext, "" + ex.message, Toast.LENGTH_LONG).show()
+                Toast.makeText(baseContext, ex.message, Toast.LENGTH_LONG).show()
             }
         }
     }
 
-    fun handleSubmitBtn(){
-        val details = getTitleDetails()
-        submitDetails(details, binding.etCollection.text.toString(), binding.etSubCollection.text.toString())
-    }
-
-    fun getTitleDetails(): TitleDetails {
+    private fun getTitleDetails(): TitleDetails {
         this.titleDetails.Title = binding.svTitle.query.toString()
         this.titleDetails.Director = binding.etDirector.text.toString()
         this.titleDetails.Actors = binding.etActors.text.toString()
@@ -62,19 +153,21 @@ class SaveActivity : AppCompatActivity(), OnItemClickListener<SearchedTitle> {
         this.titleDetails.Language = binding.etLanguage.text.toString()
         this.titleDetails.imdbRating = binding.etImdbRating.text.toString()
         return this.titleDetails
-
     }
 
-    private fun submitDetails(titleDetails: TitleDetails, collection: String, subCollection: String? = null) {
+    private fun submitDetails(
+        titleDetails: TitleDetails,
+        collection: String,
+        subCollection: String? = null
+    ) {
         var body: SaveTitleModel
-        if (subCollection.isNullOrEmpty()){
+        if (subCollection.isNullOrEmpty()) {
             body = SaveTitleModel(
                 user_id = Constants.USER_ID,
                 collection = collection,
                 details = titleDetails
             )
-        }
-        else{
+        } else {
             body = SaveTitleModel(
                 user_id = Constants.USER_ID,
                 collection = collection,
@@ -98,103 +191,5 @@ class SaveActivity : AppCompatActivity(), OnItemClickListener<SearchedTitle> {
         }
     }
 
-    private var searchJob: Job? = null
-
-    private fun svTitlesSetup() {
-        binding.svTitle.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-            override fun onQueryTextSubmit(query: String?): Boolean {
-                if (query != null) {
-                    handleSearchEvent(query)
-                }
-                return true
-            }
-
-            override fun onQueryTextChange(newText: String?): Boolean {
-                if (newText?.length!! > 0) {
-                    newText.let { text ->
-                        searchJob?.cancel() // Cancel the previous search job if any
-                        searchJob = CoroutineScope(Dispatchers.Main).launch {
-                            delay(300) // Debounce time in milliseconds
-                            handleSearchEvent(text)
-                        }
-                    }
-
-                } else {
-                    searchedTitles.removeAll { true }
-                    binding.rvSearchedTitles.adapter?.notifyDataSetChanged()
-                }
-
-                return false
-            }
-
-
-        })
-    }
-
-    private fun rvSearchedTitlesSetup(titles: MutableList<SearchedTitle>) {
-        binding.rvSearchedTitles.layoutManager = LinearLayoutManager(this)
-        binding.rvSearchedTitles.adapter = SearchedTitlesAdapter(this, this, titles)
-    }
-
-    private fun handleSearchEvent(s: String) {
-        try {
-            ApiHandler().searchTitles(s) { success, message, data ->
-                //hideLoader() // Hide loader after API call is completed
-                if (success) {
-                    if (data != null) {
-                        searchedTitles = data as MutableList<SearchedTitle>
-                        rvSearchedTitlesSetup(searchedTitles)
-                    }
-                    //Toast.makeText(baseContext, message + data, Toast.LENGTH_SHORT).show()
-                    // Proceed to next step
-                } else {
-                    Toast.makeText(baseContext, message + data, Toast.LENGTH_SHORT).show()
-                    // Handle failure case
-                }
-            }
-
-        } catch (exception: java.lang.Exception) {
-            exception.printStackTrace()
-        }
-
-    }
-
-    fun setupEts(title: TitleDetails) {
-        binding.svTitle.setQuery(title.Title, false)
-        binding.etDirector.setText(title.Director)
-        binding.etActors.setText(title.Actors)
-        binding.etYear.setText(title.Year)
-        binding.etLanguage.setText(title.Language)
-        binding.etImdbRating.setText(title.imdbRating)
-    }
-
-    private fun clearSearchedTitles() {
-        searchedTitles.clear()
-        binding.rvSearchedTitles.adapter?.notifyDataSetChanged()
-    }
-
-    override fun onItemClick(item: SearchedTitle) {
-        clearSearchedTitles()
-        fetchTitleDetails(item)
-    }
-
-    private fun fetchTitleDetails(title: SearchedTitle) {
-        ApiHandler().fetchTitleDetails(title.imdbID) { success, message, data ->
-            if (success) {
-                if (data != null) {
-                    Toast.makeText(baseContext, "hahahah" + data, Toast.LENGTH_SHORT).show()
-                    handleTitleFetchedEvent(data)
-                }
-            } else {
-                Toast.makeText(baseContext, message + data, Toast.LENGTH_SHORT).show()
-            }
-        }
-    }
-
-    fun handleTitleFetchedEvent(titleDetails: TitleDetails){
-        this.titleDetails = titleDetails
-        setupEts(titleDetails)
-
-    }
 
 }

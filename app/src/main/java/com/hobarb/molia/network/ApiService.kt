63b97f108b2cea14fa67806c5001c1ee6897ef03
dikamjit-borah <dikamjitborah.devacc01@gmail.com>
2024-04-23 00:26:42 +0530

    integrate api to fetch sub collections

--- app/src/main/java/com/hobarb/molia/network/ApiService.kt
+++ app/src/main/java/com/hobarb/molia/network/ApiService.kt
@@ -4,11 +4,15 @@ import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface MoliaBackendService {
     @POST("/v1/save")
     fun saveTitle(@Body body: SaveTitleModel): Call<JsonObject>
+
+    @GET("/v1/sub_collections/{user_id}")
+    fun fetchSubCollections(@Path("user_id") user_id: String): Call<JsonObject>
 }
 
 interface OmdbApiService {
